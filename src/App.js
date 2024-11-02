a4d0870a05a1c744b665d23e0581868349292eff
thimmeshacs <thimmeshacs160@gmail.com>
2024-11-02 18:01:27 +0530

    Check_DG

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
-// App.js
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import CheckList from "./components/CheckList";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleLogout = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      navigate("/");
+    }, 1000);
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/checklist" element={<CheckList />} />
-    </Routes>
+    <>
+      {location.pathname === "/checklist" && (
+        <button
+          onClick={handleLogout}
+          className="fixed top-5 right-5 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 transition duration-300 shadow-lg"
+        >
+          Logout
+        </button>
+      )}
+      {loading ? (
+        <div className="fixed inset-0 flex flex-col items-center justify-center bg-gray-100 bg-opacity-75">
+          <div className="relative w-12 h-12">
+            {[...Array(10)].map((_, i) => (
+              <div
+                key={i}
+                className={`absolute w-2 h-2 bg-blue-500 rounded-full animate-spin-dot`}
+                style={{
+                  transform: `rotate(${i * 36}deg) translate(20px) rotate(-${
+                    i * 36
+                  }deg)`,
+                  animationDelay: `${i * 0.3}s`,
+                }}
+              ></div>
+            ))}
+          </div>
+          <p className="mt-4 text-lg font-semibold text-gray-700">
+            Logging Out...
+          </p>
+        </div>
+      ) : (
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/checklist" element={<CheckList />} />
+        </Routes>
+      )}
+    </>
   );
 };
 
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAntHNI4o-WKe8SnJ4wXCrdpg_YiNv0N-4",
   authDomain: "canis-in-25eae.firebaseapp.com",
@@ -31,46 +31,50 @@ const LoginPage = () => {
     setIsLoading(true);
 
     try {
-      // Fetch the user credentials from Firestore
-      const docRef = doc(db, "Audit-Checklist", "RL6cOELC5DDEDoJnrD49");
-      const docSnap = await getDoc(docRef);
+      const collectionRef = collection(
+        db,
+        "Audit-Checklist/RL6cOELC5DDEDoJnrD49/New"
+      );
+      const querySnapshot = await getDocs(collectionRef);
 
-      if (docSnap.exists()) {
-        const data = docSnap.data();
+      let isValidUser = false;
+
+      querySnapshot.forEach(doc => {
+        const data = doc.data();
         const storedEmail = data["User Name"];
         const storedPassword = data["Password"];
 
-        // Check if the input email and password match the stored credentials
         if (email === storedEmail && password === storedPassword) {
-          setIsSuccess(true);
-          setMessage("Login successful!");
-          setTimeout(() => {
-            navigate("/checklist");
-          }, 1000);
-        } else {
-          setIsSuccess(false);
-          setMessage("Invalid email or password.");
+          isValidUser = true;
         }
+      });
+
+      if (isValidUser) {
+        setIsSuccess(true);
+        setMessage("Login successful!");
+        setTimeout(() => {
+          navigate("/checklist");
+        }, 1000);
       } else {
         setIsSuccess(false);
-        setMessage("No credentials found.");
+        setMessage("Invalid email or password.");
       }
     } catch (error) {
       setIsSuccess(false);
       setMessage("An error occurred. Please try again.");
-      console.error("Error fetching document: ", error);
+      console.error("Error fetching documents: ", error);
     } finally {
       setIsLoading(false);
     }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      <div className="w-full max-w-sm bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-3xl font-bold text-center mb-6 text-gray-800">
+    <div className="flex items-center justify-center w-full min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-100">
+      <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg bg-white shadow-lg rounded-lg p-6 md:p-8 lg:p-10">
+        <h2 className="text-2xl md:text-3xl font-bold text-center mb-4 text-black-800">
           Login
         </h2>
-        <form onSubmit={handleSubmit} className="space-y-6">
+        <form onSubmit={handleSubmit} className="space-y-5">
           <div>
             <label
               htmlFor="email"
@@ -83,7 +87,7 @@ const LoginPage = () => {
               id="email"
               value={email}
               onChange={e => setEmail(e.target.value)}
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
               placeholder="Enter your email"
               required
             />
@@ -100,47 +104,45 @@ const LoginPage = () => {
               id="password"
               value={password}
               onChange={e => setPassword(e.target.value)}
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
               placeholder="Enter your password"
               required
             />
           </div>
-          <div>
-            <button
-              type="submit"
-              className="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 flex items-center justify-center"
-              disabled={isLoading}
-            >
-              {isLoading ? (
-                <svg
-                  className="animate-spin h-5 w-5 text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-              ) : (
-                "Submit"
-              )}
-            </button>
-          </div>
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white py-3 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 flex items-center justify-center"
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <svg
+                className="animate-spin h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            ) : (
+              "Submit"
+            )}
+          </button>
         </form>
         {message && (
           <p
-            className={`mt-4 text-center ${
+            className={`mt-6 text-center ${
               isSuccess ? "text-green-500" : "text-red-500"
             }`}
           >
